3f7ab6f33b6b97a592a5415aedc1cb262d3bb9c8
haris_delalic <haris.delalic@datamolino.com>
2022-12-31 20:39:52 +0100

    add rich text editor

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,6 @@ require("stylesheets/application.scss")
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require("trix")
+require("@rails/actiontext")
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,6 @@
 class Course < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true, length: { minimum: 5, maximum: 255 }
+
+  has_rich_text :description
 end
